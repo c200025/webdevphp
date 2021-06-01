c087b8ae498df32597db0dc16198800ca522f8be
c200025 <c200025@ccg.ac.jp>
2021-06-01 06:59:01 +0000

    lec-04

---
+++ lec-04/source.php
@@ -0,0 +1,17 @@
+<?php
+$scores = [];
+$scores[] = 81; // 国語
+$scores[] = 79; // 数学
+$scores[] = 78; // 英語
+$scores[] = 74; // 社会
+$scores[] = 80; // 理科
+
+if ($scores[2] >= 80) {
+  echo "合格";
+} else {
+  echo "不合格";
+}
+?>
+
+
+
